3510774f0a7b57e7e2c23f64e3f53f1efb427f6e
Nashe Omirro <76786181+nasheomirro@users.noreply.github.com>
2025-04-29 22:38:04 +0800

    downgraded from ES2022 and fixed scripts

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,28 @@
-type Map = {
-  [k: string]: {
-    [k: string]: any;
-  };
-};
+type Variants = { [k: string]: { [k: string]: any } };
 
-type DefaultKeys<T extends Map, K extends keyof T = keyof T> = K extends any ? ("$" extends keyof T[K] ? K : never) : never;
+type DefaultKeys<T extends Variants, K extends keyof T = keyof T> = K extends any ? ("$" extends keyof T[K] ? K : never) : never;
+type RequiredKeys<T extends Variants> = Exclude<keyof T, DefaultKeys<T>>;
 
-type RequiredKeys<T extends Map> = Exclude<keyof T, DefaultKeys<T>>;
-
-export type OptionProps<T extends Map, P extends any> = P & {
+export type MapWith<T extends Variants> = {
   [K in DefaultKeys<T>]?: Exclude<keyof T[K], "$">;
 } & {
   [K in RequiredKeys<T>]: keyof T[K];
 };
 
-export function choose<T extends Map>(options: T, props: any): { [K in keyof T]: T[K] } {
-  let _props: any = {};
+export type PropsMap<T extends Variants, P extends any> = P & MapWith<T>;
 
+export function map<T extends Variants>(options: T, props: any): { [K in keyof T]: T[K][Exclude<keyof T[K], "$">] } {
+  let _props: any = {};
   for (let key of Object.keys(options)) {
-    const fallback = options[key]["$"];
-    if (Object.hasOwn(props, key)) {
+    if (Object.prototype.hasOwnProperty.call(props, key)) {
+      // if props has a key, use that key to determine which value should be mapped
       _props[key] = options[key][props[key]];
-    } else if (typeof fallback !== "undefined") {
-      _props[key] = fallback;
+    } else if (Object.prototype.hasOwnProperty.call(options[key], "$")) {
+      // if props doesn't have it, use the fallback key in '$'
+      const fbKey = options[key]["$"];
+      _props[key] = options[fbKey];
     } else {
+      // if props does not hold the key and no fallback was found, throw an error
       throw new Error(`value for ${key} has not been provided and does not have a fallback`);
     }
   }
