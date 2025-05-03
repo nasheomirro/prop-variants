01f1189433426a61e35f3640340cfcbf68b1e0bb
Nashe Omirro <76786181+nasheomirro@users.noreply.github.com>
2025-05-03 10:21:20 +0800

    removed the  property and modified all types to reflect that

---
+++ .changeset/eager-ravens-lie.md
@@ -0,0 +1,10 @@
+---
+"prop-variants": minor
+---
+
+Breaking changes:
+
+- Removed the `$def` property
+- `ToVariants`, `ToKeyMap`, and `ToValueMap` now all function like their `basic` counterparts
+- Removed `basic` types and moved them to their core counterparts
+  `Removed`ToCompedValueMap`
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export type AnyObject = { [K: string]: any };
-export type VariantGroup = { [K: string]: AnyObject };
+type AnyObject = { [K: string]: any };
+type VariantGroup = { [K: string]: AnyObject };
 
 /**
  * Checks if type is an actual string instance `(ex. "foo" | "bar" | "baz")` and not just `string`.
@@ -14,22 +14,11 @@ type GetVariantKeys<T extends AnyObject, K extends keyof T = keyof T> = K extend
   : never;
 
 /**
- * Returns only the list of variants that have a default key
+ * Takes the keys that could be undefined from the given `T`.
  */
-type GetVariantsWithDef<T extends VariantGroup, D extends string, K extends keyof T = keyof T> = K extends any
-  ? D extends keyof T[K]
+export type GetOptionalKeys<T extends AnyObject, K extends keyof T = keyof T> = K extends any
+  ? undefined extends T[K]
     ? K
-    : never
-  : never;
-
-/**
- * Returns only the list of variants that have a default key that is `null` or `undefined`
- */
-type GetVariantsWithDefNull<T extends VariantGroup, D extends string, K extends keyof T = keyof T> = K extends any
-  ? D extends keyof T[K]
-    ? T[K][D] extends null | undefined
-      ? K
-      : never
     : never
   : never;
 
@@ -38,49 +27,7 @@ type GetVariantsWithDefNull<T extends VariantGroup, D extends string, K extends 
  *
  * Always use the `satisfies` keyword instead of directly typing or asserting this to your object.
  */
-export type ToVariants<T extends AnyObject, D extends string = "$def"> = {
-  [K in GetVariantKeys<T>]-?: {
-    [L in Exclude<T[K], undefined>]: any;
-  } & (undefined extends T[K]
-    ? {
-        [L in D]?: T[K] | undefined | null;
-      }
-    : {});
-};
-
-/**
- * Constructs a set of key-value pairs using the structure of `T`.
- *
- * You must be explicit with your use of `D` in `T` to correctly signal that the key should be optional.
- */
-export type ToKeyMap<T extends VariantGroup, D extends string = "$def"> = {
-  [L in GetVariantsWithDef<T, D>]?: Exclude<keyof T[L], D>;
-} & {
-  [L in Exclude<keyof T, GetVariantsWithDef<T, D>>]: Exclude<keyof T[L], D>;
-};
-
-/**
- * Like `ToKeyMap` but instead of giving keys for each variant, it gives you the values that they might have.
- */
-export type ToValueMap<T extends VariantGroup, D extends string = "$def"> = {
-  [K in keyof T]: T[K][Exclude<keyof T[K], D>] | (K extends GetVariantsWithDef<T, D> ? undefined : never);
-};
-
-/**
- * Like `ToValueMap` but the values assume that default key would be used if value could be `undefined`, if
- * default is null, it remains `undefined`.
- */
-export type ToCompedValueMap<T extends VariantGroup, D extends string = "$def"> = {
-  [K in keyof T]: T[K][Exclude<keyof T[K], D>] | (K extends GetVariantsWithDefNull<T, D> ? undefined : never);
-};
-
-/**
- * From `T`, tries it's best to determine which are variants and returns a variant group based on it. Does not have
- * default keys.
- *
- * Always use the `satisfies` keyword instead of directly typing or asserting this to your object.
- */
-export type ToBasicVariants<T extends AnyObject> = {
+export type ToVariants<T extends AnyObject> = {
   [K in GetVariantKeys<T>]-?: {
     [L in Exclude<T[K], undefined>]: any;
   };
@@ -89,17 +36,19 @@ export type ToBasicVariants<T extends AnyObject> = {
 /**
  * Constructs a set of key-value pairs using the structure of `T`.
  *
- * Because we don't use default keys, you could instead provide a list of variants you want to be optional.
+ * Because we don't have an intrinsic way to tell which variant is optional,
+ * you could instead provide a list of variants you want to be optional.
  */
-export type ToBasicKeyMap<T extends VariantGroup, Optional extends keyof T = never> = {
+export type ToKeyMap<T extends VariantGroup, Optional extends keyof T = never> = {
   [K in Exclude<keyof T, Optional>]: keyof T[K];
-} & (Optional extends keyof T ? { [K in Optional]?: keyof T[K] | undefined } : never);
+} & ([Optional] extends [keyof T] ? { [K in Optional]?: keyof T[K] | undefined } : {});
 
 /**
  * Like `ToKeyMap` but instead of giving keys for each variant, it gives you the values that they might have.
  *
- * Because we don't use default keys, you could instead provide a list of variants that could be `undefined`
+ * Because we don't have an intrinsic way to tell which variant is optional,
+ * you could instead provide a list of variants you want to be optional.
  */
-export type ToBasicValueMap<T extends VariantGroup, Optional extends keyof T = never> = {
-  [K in Exclude<keyof T, Optional>]: T[K][keyof T[K]];
-} & (Optional extends keyof T ? { [K in Optional]: T[K][keyof T[K]] | undefined } : never);
+export type ToValueMap<T extends VariantGroup, Optional extends keyof T = never> = {
+  [K in Exclude<keyof T, Optional | undefined>]: T[K][keyof T[K]];
+} & ([Optional] extends [keyof T] ? { [K in Exclude<Optional, undefined>]: T[K][keyof T[K]] | undefined } : {});
